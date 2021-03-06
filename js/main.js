2269b5d634e1ccbf33570205f89a175cdcc3b235
Manuel Congosto <mc@y2kwebs.com>
2021-03-06 14:32:05 +0100

    Exercici 1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+async function getAcudit() {
+  await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
