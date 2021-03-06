db958cc9ff225af0dee972e3da0e86e78697612e
Manuel Congosto <mc@y2kwebs.com>
2021-03-06 16:33:30 +0100

    Exercici 4

--- js/main.js
+++ js/main.js
@@ -13,17 +13,18 @@ async function getAcudit() {
 async function getTiempo() {
   let str = "";
   await fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=6c35df700d93402106941f4d54f5a5a1"
+    "http://api.openweathermap.org/data/2.5/weather?q=barcelona&units=metric&appid=6c35df700d93402106941f4d54f5a5a1"
   )
     .then((response) => response.json())
     .then((data) => {
-      str += `Temperatura: ${data.main.temp} - `;
-      str += `Mínima: ${data.main.temp_min} - `;
-      str += `Máxima: ${data.main.temp_max} - `;
+      str += `Temp: ${data.main.temp}° - `;
+      str += `Mínima: ${data.main.temp_min}° - `;
+      str += `Máxima: ${data.main.temp_max}° - `;
       str += `Humedad: ${data.main.humidity}`;
+      str += `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"></img>`;
     });
 
-  document.getElementById("tiempo").innerHTML = str;
+  document.getElementById("tiempo").innerHTML = "Avui a Barcelona: " + str;
 }
 
 getTiempo();
